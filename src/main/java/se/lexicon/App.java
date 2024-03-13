3b32e24dad6f342e74eb0d30d32171987cb73ead
Elnaz <azizi.elnaz66@gmail.com>
2024-03-13 12:03:58 +0100

    Part 3

--- src/main/java/se/lexicon/App.java
+++ src/main/java/se/lexicon/App.java
@@ -2,12 +2,15 @@ package se.lexicon;
 
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        System.out.println(NameRepository.getSize());
+        boolean isAddElnaz = NameRepository.add("Elnaz Azizi");
+        System.out.println(isAddElnaz);
+        System.out.println(NameRepository.getSize()); // 1
+
+
+
     }
 }
--- src/main/java/se/lexicon/NameRepository.java
+++ src/main/java/se/lexicon/NameRepository.java
@@ -4,15 +4,16 @@ import java.sql.Array;
 import java.util.Arrays;
 
 public class NameRepository {
-    private static String[] names = new String[0];
+    private static String[] names = new String[0]; // storage - memory - where you can store all the names
+
 
     public static int getSize() {
         return names.length;
     }
 
+    //
     public static void setNames(String[] newNames) {
         names = Arrays.copyOf(newNames, newNames.length);
-
     }
 
     public static void clear() {
@@ -41,9 +42,37 @@ public class NameRepository {
         return true;
     }
 
-    public static void main(String[] args) {
+    /* public static String[] findByFirstName(final String firstName)
+Searches the array trying to find all names that has passed in first name. Returns a String array containing all
+matches.*/
+    public static String[] findByFirstName(final String firstName) {
+        String[] matches = new String[0];
+        for (String name : names) {
+            if (name.startsWith(firstName + " ")) {
+                matches = Arrays.copyOf(matches, matches.length + 1);
+                matches[matches.length - 1] = name;
+            }
+        }
+        return matches;
+    }
+    /*public static String[] findByLastName(final String lastName)
+Searches the array trying to find all names that has passed in last name. Returns a String array containing all
+matches.*/
 
-    }//main
+    public static String[] findByLastName(final String lastName) {
+        String[] matches = new String[0];
+        for (String name : names) {
+            if (name.endsWith(" " + lastName)) {
+                matches = Arrays.copyOf(matches, matches.length + 1);
+                matches[matches.length - 1] = name;
+            }
+        }
+        return matches;
+    }
+    /* public static boolean update(final String original, final String updatedName)
+Should find a name and replace it with new fullName if available. Returns true if name was found and updated
+with the new name. False if name could not be updated because name wasn’t found or name was found but an
+existing name matching the updatedName already exists */
 
 
 }//class
